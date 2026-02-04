c84b8c0056fe97fb554d71a30414322004798ec7
jk-rieck <jan.klaus.rieck@gmail.com>
2026-02-04 14:23:21 -0500

    Update decode_convert.sh

--- decode_convert.sh
+++ decode_convert.sh
@@ -10,4 +10,4 @@ source /opt/anaconda3/etc/profile.d/conda.sh
 conda activate /opt/anaconda3/envs/buoy-data
 
 # run python script to decode and convert data
-python3 /storage/common/buoy-data/convert-buoy-data/decode_convert_300534065720080/decode_convert.py
+python3 /storage/common/buoy-data/convert-buoy-data/decode_convert.py
